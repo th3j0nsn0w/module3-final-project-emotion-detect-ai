4f2ec2afcd4691ad065d05f756f0931f6322d159
th3j0nsn0w <branstarkjonsnow@gmail.com>
2025-10-31 00:12:25 -0400

    Adding server to connect front-end with emotion service

---
+++ server.py
@@ -0,0 +1,40 @@
+'''
+Server using Flask.
+'''
+
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detector")
+
+@app.route("/emotionDetector")
+def detect_emotion():
+    '''
+    Process text to detect the dominant emotion
+    '''
+    text_to_analyze = request.args.get('textToAnalyze')
+    if text_to_analyze:
+        emotion_info = emotion_detector(text_to_analyze)
+        if emotion_info:
+            emotion_response = (
+                f"For the given statement, the system response is 'anger': "
+                f"{ emotion_info['anger'] }, 'disgust': { emotion_info['disgust'] }, 'fear': "
+                f"{ emotion_info['fear'] }, 'joy': { emotion_info['joy'] } and 'sadness': "
+                f"{ emotion_info['sadness'] }. The dominant emotion is "
+                f"{ emotion_info['dominant_emotion'] }."
+            )
+
+            return emotion_response
+
+    return "Text is not valid." if text_to_analyze else "Empty text! Nothing to analyze."
+
+@app.route("/")
+def render_index_page():
+    '''
+    Render main application page
+    '''
+
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
